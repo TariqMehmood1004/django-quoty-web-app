3e261c4df27c28c83c4187226bf9fae9319724d4
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2024-10-12 18:00:10 +0500

    final version

--- QuotyProjectApp/urls.py
+++ QuotyProjectApp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path(route='', view=views.index, name='index'),
+    path(route='details/', view=views.details, name='details'),
+    path(route='pay/', view=views.pay, name='pay'),
+    path(route='contact/', view=views.contact, name='contact'),
 ]
