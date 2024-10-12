90260eed3ab0795e101c87e7015903ebdd04a066
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2024-10-13 00:37:50 +0500

    added all the assets to static folder

--- QuotyProject/settings.py
+++ QuotyProject/settings.py
@@ -102,11 +102,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 
 # Media
 MEDIA_ROOT = BASE_DIR / 'media'
--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,4 @@
 echo "BUILD START"
-
-#!/bin/bash
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
 python3 manage.py collectstatic --noinput
-
-
  echo "BUILD END"
